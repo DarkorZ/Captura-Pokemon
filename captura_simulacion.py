087ed94bbbf7289f23002304f2e3337d2e0e2bdd
DarkorZ <jcristofer13@gmail.com>
2025-07-12 23:29:34 -0500

    Generador de pokemon salvaje

--- captura_simulacion.py
+++ captura_simulacion.py
@@ -1,11 +1,20 @@
 import requests #para hacer solicitudes HTTP a la api
 
+import random #libreria para randomizar numeros
+import time
+import os
+
+def limpiar_consola():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def obtener_datos_pokemon(nombre):
 
-    nombre = nombre.lower()
+    nombre = str(nombre).lower()
     url_pokemon = f'https://pokeapi.co/api/v2/pokemon/{nombre}' #llamamos con la direccion de api oficial de pokeapi, al nombre que vamos a ingresar en la funcion
     url_species = f'https://pokeapi.co/api/v2/pokemon-species/{nombre}' #llamamos con el mismo nombre, la especie, segun el pokeapi aqui se encuentra el capture rate
+
+    #avance 2
+    
     r_pokemon = requests.get(url_pokemon) #solicitudes HTTP para obtener los datos
     r_species = requests.get(url_species) #solicitudes http para obtener datos de la especie donde se clasifica el capture rate
 
@@ -18,7 +27,8 @@ def obtener_datos_pokemon(nombre):
     #almacenamos el json reibido
     data_pokemon=r_pokemon.json()
     data_species=r_species.json()
-    print(data_species.keys())
+    
+    #print(data_species.keys())
     
     #para extraer el valor clave para nuestro proyecto el HP, mandamos un for que recorra la lista de stats 
 
@@ -36,17 +46,62 @@ def obtener_datos_pokemon(nombre):
     if capture_rate is None:
         raise Exception("NO HAY CAPTURE RATE")
 
-    return hp, capture_rate 
 
-if __name__ == "__main__":
-    nombre = input("ingrese el nombre del pokemon  ")
+    #obtenermos el ID del pokemon para nuestra generacion aleatoria
+    id_pokemon = data_pokemon['id']
 
+    nombre = data_pokemon['name']
+
+    #agregamos los dos nuevos valores al return
+    return hp, capture_rate,  id_pokemon,  nombre
+
+
+def generate_pkm(): #Funcion de generacion de pokemon aleatoria
+    num_r=random.randint(1,151)
+    return obtener_datos_pokemon(num_r)
+
+   
+        
+
+
+def play():
+
+    
     try:
-        hp, capture_rate=obtener_datos_pokemon(nombre)
-        print(f"pokemon: {nombre.capitalize()}")
-        print(f"HP base: {hp}")
+        pasos= int(input("Cuantos pasos deseas avanzar?: "))
+        if pasos <=0 :
+            print("Pasos invalidos, ingresa nuevamente")
+            return
+    except ValueError:
+        print("Entrada incorrecta. ")
+        return
+    
+    print(f"Entrenador RED avanzara {pasos} pasos...")
+    print("Ha aparecido un POKEMON salvaje! ")
+    time.sleep(1.5)
 
-        print(f"Capture rate: {capture_rate}")
+    
+    hp, capture_rate, id_pokemon, nombre =generate_pkm()
+    print(f"#Pokedex: {id_pokemon}")
+        
+    print(f"pokemon: {nombre.capitalize()}")
+    print(f"HP base: {hp}")
 
-    except Exception as e: #salimos del try para imprimir el error nque pueda mostrar
-        print(str(e))
+    print(f"Capture rate: {capture_rate}")
+    time.sleep(2.5)
+    limpiar_consola()
+def menu():
+    while True:
+        print("Simulador de captura pokemon")
+        opcion = input("Avanzar? (s/n) ")
+        if opcion == 's':
+            play()
+        elif opcion == 'n':
+            print("Fin del simulador")
+        else:
+            print("Entrada incorrecta")
+            
+if __name__ == "__main__":
+    menu()
+
+
